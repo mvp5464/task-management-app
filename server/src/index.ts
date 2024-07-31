c041b7b9c90df1183110f741b36394df2ee9c8da
mvking5464 <mvking5464@gmail.com>
2024-07-31 15:15:38 +0530

    solved build error18

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,13 @@ import { taskRoute } from "./routes/task";
 
 const router = express.Router();
 const PORT = dotenv.parsed.PORT;
+const mongoUrl = dotenv.parsed.MONGO_URI;
+const myPort = dotenv.parsed.PORT;
+const myJwt = dotenv.parsed.JWT_SECRET;
+
+console.log({ myPort });
+console.log({ myJwt });
+console.log({ mongoUrl });
 
 app.use(cors());
 app.use(express.json());
