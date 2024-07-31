3f625f9fbc41ce196b6697cac046bba197327bb7
mvking5464 <mvking5464@gmail.com>
2024-08-01 00:47:10 +0530

    added hover effects 2

--- client/src/components/SideSection.tsx
+++ client/src/components/SideSection.tsx
@@ -4,7 +4,7 @@ const SideSection = ({ icon, name }: { icon: JSX.Element; name: string }) => {
   return (
     <>
       <div
-        className={`flex gap-2 w-full px-2 py-1 text-[#797979] ${
+        className={`flex gap-2 w-full px-2 py-1 text-[#797979] hover:shadow-lg hover:bg-neutral-100 ${
           name === "Home" && "bg-[#F4F4F4] border border-[#DDDDDD] rounded-sm"
         } `}
       >
